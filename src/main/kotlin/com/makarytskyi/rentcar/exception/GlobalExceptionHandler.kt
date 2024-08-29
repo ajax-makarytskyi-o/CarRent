5c83438478b3028084a2882ef664c0227860de7f
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-08-29 16:33:02 +0300

    removed time field in error response

--- src/main/kotlin/com/makarytskyi/rentcar/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/makarytskyi/rentcar/exception/GlobalExceptionHandler.kt
@@ -1,6 +1,5 @@
 package com.makarytskyi.rentcar.exception
 
-import java.time.LocalDateTime
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -11,7 +10,6 @@ internal class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException::class)
     internal fun handleResourceNotFoundException(): ResponseEntity<ErrorResponse> {
         val errorResponse = ErrorResponse(
-            LocalDateTime.now(),
             HttpStatus.NOT_FOUND.value(),
         )
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse)
@@ -20,14 +18,12 @@ internal class GlobalExceptionHandler {
     @ExceptionHandler(IllegalArgumentException::class)
     internal fun handleIllegalArgumentException(): ResponseEntity<ErrorResponse> {
         val errorResponse = ErrorResponse(
-            LocalDateTime.now(),
             HttpStatus.BAD_REQUEST.value(),
         )
         return ResponseEntity(errorResponse, HttpStatus.BAD_REQUEST)
     }
 
     internal data class ErrorResponse(
-        val timestamp: LocalDateTime,
         val status: Int,
     )
 }

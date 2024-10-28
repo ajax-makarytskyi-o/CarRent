8c8b9f3bcb249fa6bea02cc53118d76e008d0d0b
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-10-28 15:44:42 +0200

    internal api module added

---
+++ internal-api/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("kotlin-conventions")
+    id("com.google.protobuf") version "0.9.4"
+}
+
+dependencies {
+    api("com.google.protobuf:protobuf-java:3.24.3")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.24.3"
+    }
+}

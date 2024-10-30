5b700ab48e4c75d78fab06a8ebb8dbcb955cb938
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-10-30 14:08:08 +0200

    refactored internal api

--- internal-api/src/main/kotlin/com/makarytskyi/internalapi/subject/NatsSubject.kt
+++ internal-api/src/main/kotlin/com/makarytskyi/internalapi/subject/NatsSubject.kt
@@ -2,11 +2,12 @@ package com.makarytskyi.internalapi.subject
 
 object NatsSubject {
     object Order {
-        const val ORDER_BASE = "order"
-        const val CREATE = ORDER_BASE + ".create"
-        const val PATCH = ORDER_BASE + ".patch"
-        const val DELETE = ORDER_BASE + ".delete"
-        const val FIND_BY_ID = ORDER_BASE + ".find_by_id"
-        const val FIND_ALL = ORDER_BASE + ".find_all"
+        private const val ORDER_BASE = "order"
+
+        const val CREATE = "$ORDER_BASE.create"
+        const val PATCH = "$ORDER_BASE.patch"
+        const val DELETE = "$ORDER_BASE.delete"
+        const val FIND_BY_ID = "$ORDER_BASE.find_by_id"
+        const val FIND_ALL = "$ORDER_BASE.find_all"
     }
 }

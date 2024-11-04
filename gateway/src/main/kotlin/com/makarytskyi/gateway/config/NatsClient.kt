d2465a61d4ba2afbd0b8d5d6b5be156bffe9b6d2
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-11-04 13:28:24 +0200

    changed nats client with using request with timeout

--- gateway/src/main/kotlin/com/makarytskyi/gateway/config/NatsClient.kt
+++ gateway/src/main/kotlin/com/makarytskyi/gateway/config/NatsClient.kt
@@ -3,19 +3,26 @@ package com.makarytskyi.gateway.config
 import com.google.protobuf.GeneratedMessage
 import com.google.protobuf.Parser
 import io.nats.client.Connection
+import java.time.Duration
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 
 @Component
-class NatsClient(private val natsConnection: Connection) {
+class NatsClient(
+    private val natsConnection: Connection,
+    @Value("\${nats.timeout}")
+    private val timeoutDuration: Long
+) {
     fun <RequestT : GeneratedMessage, ResponseT : GeneratedMessage> request(
         subject: String,
         data: RequestT,
-        parser: Parser<ResponseT>
+        parser: Parser<ResponseT>,
     ): Mono<ResponseT> = Mono.fromFuture {
-        natsConnection.request(
+        natsConnection.requestWithTimeout(
             subject,
-            data.toByteArray()
+            data.toByteArray(),
+            Duration.ofSeconds(timeoutDuration)
         )
     }.map { parser.parseFrom(it.data) }
 }

b055072e94aa1dc391606af23474962bd10af6bd
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-11-19 12:39:27 +0200

    fixed parsing error

--- domainservice/src/main/kotlin/com/makarytskyi/rentcar/kafka/CreateRepairingKafkaProcessor.kt
+++ domainservice/src/main/kotlin/com/makarytskyi/rentcar/kafka/CreateRepairingKafkaProcessor.kt
@@ -4,9 +4,11 @@ import com.makarytskyi.internalapi.commonmodels.repairing.Repairing
 import com.makarytskyi.rentcar.mapper.OrderMapper.toNotification
 import com.makarytskyi.rentcar.service.OrderService
 import com.makarytskyi.rentcar.util.timestampToDate
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
 import reactor.kafka.receiver.KafkaReceiver
 import reactor.kotlin.core.publisher.toMono
 
@@ -21,7 +23,17 @@ class CreateRepairingKafkaProcessor(
     fun consume() {
         createRepairingKafkaReceiver.receive()
             .flatMap { record ->
-                Repairing.parser().parseFrom(record.value()).toMono()
+                record.value().toMono()
+                    .map { Repairing.parser().parseFrom(it) }
+                    .onErrorResume {
+                        log.atError()
+                            .setMessage("Error happened during parsing: {}")
+                            .addArgument(it.message)
+                            .setCause(it)
+                            .log()
+
+                        Mono.empty()
+                    }
                     .flatMap {
                         orderService.findOrderByCarAndDate(it.carId, timestampToDate(it.date))
                             .doOnNext { order -> orderService.deleteById(order.id) }
@@ -30,5 +42,9 @@ class CreateRepairingKafkaProcessor(
                     .doFinally { record.receiverOffset().acknowledge() }
             }
             .subscribe()
+    }
+
+    companion object {
+        val log = LoggerFactory.getLogger(CreateRepairingKafkaProcessor::class.java)
     }
 }
